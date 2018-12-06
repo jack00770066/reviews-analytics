42f2d80a80e48b85054c4d8252d92573bb1c033a
JackWang <jack00770066@gmail.com>
2018-12-06 22:06:04 +0800

    uadate good sorting

--- read_review.py
+++ read_review.py
@@ -12,3 +12,16 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('平均是: ', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '留言提到')
+print(good[0])
